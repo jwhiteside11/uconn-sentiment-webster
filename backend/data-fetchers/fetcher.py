5ca347203fe21e5b4276f75b867e84e02ba2384b
John Whiteside <johnwhitesidewebdev@gmail.com>
2025-04-19 17:00:32 -0400

    refactor category score

--- backend/data-fetchers/fetcher.py
+++ backend/data-fetchers/fetcher.py
@@ -34,89 +34,24 @@ class Fetcher:
       for url in urls:
         res = fetch_news.scrape_news_story(url)
         if "error" in res:
-          print(f"scrape failed: {url}", res['error'])
           results.append({"message": f"ERROR {res['error']}"})
           continue
 
-        print(f"scraped: {url}")
-        text = '\n'.join(res["paragraphs"])
-        # score using FinBERT model
-        score_res = self.model.score_text(text)
-        updated_doc = CategoryNewsDocument(ticker=ticker, score=score_res["score"], magnitude=score_res["magnitude"], **res)
-        # Generate keywords for the story
-        updated_doc.keywords = generate_keywords.generate_keywords_sorted(''.join(text))
+        news_doc = CategoryNewsDocument(ticker=ticker, **res)
+        res = self.category_score_news_doc(news_doc)
 
-        # Calculate scores for each paragraph
-        paragraph_kws = []
-        total_magnitude = 0
-        weighted_sum = 0
+        if "error" in res:
+          results.append({"message": f"ERROR {res['error']}"})
+          continue
 
-        # Loop through paragraphs and calculate scores 
-        for paragraph in res["paragraphs"]:
-          if len(paragraph) < 10: # disregard inputs less than 10 characters - irrelevant to sentiment
-            continue
-
-          # Generate keywords for the paragraph
-          p_keywords = generate_keywords.generate_keywords_sorted(paragraph)
-          # Generate categorial score for the story
-          # score using FinBERT model
-          score_res = self.model.score_text(paragraph)
-          if "error" in score_res:
-            return {"messsage": f"ERROR {score_res['error']}"}
-
-          sentiment_score = score_res["score"]
-          magnitude = score_res["magnitude"]
-
-          # Add to the total magnitude and weighted sum
-          total_magnitude += magnitude
-          weighted_sum += sentiment_score * magnitude
-              
-          # Append paragraph score details
-          paragraph_kws.append({
-            "score": sentiment_score,
-            "magnitude": magnitude,
-            "keywords": p_keywords
-          })
-
-        # Calculate the weighted average score
-        if total_magnitude > 0:
-            weighted_average_score = weighted_sum / total_magnitude
-        else:
-            weighted_average_score = 0
-
-        updated_doc.score = weighted_average_score
-        updated_doc.weighted_score = weighted_sum
-        updated_doc.magnitude = total_magnitude
-        updated_doc.paragraph_kws = paragraph_kws
-
-        keywords = updated_doc.keywords
-        for score_obj in paragraph_kws:
-          for cat in score_obj["keywords"].keys():
-            if cat not in keywords:
-              existing_kw = score_obj["keywords"][cat]["keywords"]
-              keywords[cat] = {"count": len(existing_kw), "keywords": existing_kw, "score": 0, "weighted_score": 0, "magnitude": 0}
-
-            keywords[cat]["weighted_score"] += score_obj["score"] * score_obj["magnitude"]
-            keywords[cat]["magnitude"] += score_obj["magnitude"]
-
-        for cat in keywords.keys():
-          if keywords[cat]["magnitude"] > 0:
-            keywords[cat]["score"] = keywords[cat]["weighted_score"] / keywords[cat]["magnitude"]
-          else:
-            keywords[cat]["score"] = 0
-
-        # save document to datastore
-        self.ds.createNewsStoryEntity(updated_doc)
-        
-        # index into typesense
+        # attempt index into typesense
         try:
-          self.ts.createNewsDocument(updated_doc)
-          print("added: ", updated_doc.url)
+          self.ts.createNewsDocument(news_doc)
         except Exception as e:
-          print("failed: ", updated_doc.url, e)
+          print("failed: ", news_doc.url, e)
 
         # aggregate scrape results
-        results.append({"message": f"SUCCESS {updated_doc.url} score: {updated_doc.score} magnitude: {updated_doc.magnitude}"})
+        results.append(res)
 
     return results
   
@@ -139,9 +74,18 @@ class Fetcher:
     if len(news_document.paragraphs) == len(news_document.paragraph_kws):
       return {"message": f"ERROR Document has already been scored"}
     
-    updated_doc = CategoryNewsDocument(**(news_document.__dict__))
+    res = self.category_score_news_doc(news_document)
+
+    if "error" in res:
+      return {"messsage": f"ERROR {res['error']}"}
+    
+    return res
+
+  def category_score_news_doc(self, news_doc: CategoryNewsDocument):
+    # filter paragraphs to exclude meaningless statements
+    paragraphs = [p for p in news_doc.paragraphs if len(p) >= 10]
     # Generate keywords for the story
-    updated_doc.keywords = generate_keywords.generate_keywords_sorted(''.join(news_document.paragraphs))
+    keywords = generate_keywords.generate_keywords_sorted(''.join(news_doc.paragraphs))
 
     # Calculate scores for each paragraph
     paragraph_kws = []
@@ -149,17 +93,14 @@ class Fetcher:
     weighted_sum = 0
 
     # Loop through paragraphs and calculate scores 
-    for paragraph in news_document.paragraphs:
-      if len(paragraph) < 10: # disregard inputs less than 10 characters - irrelevant to sentiment
-        continue
-
+    for paragraph in paragraphs:
       # Generate keywords for the paragraph
       p_keywords = generate_keywords.generate_keywords_sorted(paragraph)
       # Generate categorial score for the story
       # score using FinBERT model
       score_res = self.model.score_text(paragraph)
       if "error" in score_res:
-        return {"messsage": f"ERROR {score_res['error']}"}
+        return score_res
 
       sentiment_score = score_res["score"]
       magnitude = score_res["magnitude"]
@@ -181,14 +122,14 @@ class Fetcher:
     else:
         weighted_average_score = 0
 
-    updated_doc.score = weighted_average_score
-    updated_doc.weighted_score = weighted_sum
-    updated_doc.magnitude = total_magnitude
-    updated_doc.paragraph_kws = paragraph_kws
+    news_doc.score = weighted_average_score
+    news_doc.weighted_score = weighted_sum
+    news_doc.magnitude = total_magnitude
+    news_doc.paragraph_kws = paragraph_kws
+    news_doc.paragraphs = paragraphs
+    news_doc.keywords = keywords
 
-    keywords = updated_doc.keywords
-
-    # use paragrah scores to form article score
+    # use paragrah scores to form category scores
     for score_obj in paragraph_kws:
       for cat in score_obj["keywords"].keys():
         if cat not in keywords or "score" not in keywords[cat]:
@@ -205,10 +146,10 @@ class Fetcher:
         keywords[cat]["score"] = 0
 
     # save document to datastore
-    self.ds.createNewsStoryEntity(updated_doc)
+    self.ds.createNewsStoryEntity(news_doc)
 
     # Return the aggregated results
-    return {"message": f"SUCCESS {updated_doc.url} score: {updated_doc.score} magnitude: {updated_doc.magnitude}"}
+    return {"message": f"SUCCESS {news_doc.url} score: {news_doc.score} magnitude: {news_doc.magnitude}"}
   
 
   def scrape_earnings_calls(self, ticker: str):
